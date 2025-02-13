0ef4012e5b0e670ea6400ae6242c7804037b98ec
blueeboy <148228902+blueeboy@users.noreply.github.com>
2025-02-13 19:28:52 +0100

    Update covers.js

--- covers.js
+++ covers.js
@@ -1,14 +1,15 @@
-const video = document.querySelectorAll(".video")
-const vid = document.querySelectorAll("video")
+const videos = document.querySelectorAll('.video-bg');
 
-
-video.addEventListener("click", () => {
-    video.forEach((vid) => {
-        vid.play();
-        vid.setAttribute("controls", "controls")
-    })
-    
-})
+videos.forEach(video => {
+    video.addEventListener('play', () => {
+        // Pause all other videos when one is playing
+        videos.forEach(v => {
+            if (v !== video) {
+                v.pause();
+            }
+        });
+    });
+});
 
 
 // function toggleVideo() {
@@ -18,6 +19,43 @@ video.addEventListener("click", () => {
 //         video.pause();
 //     }
 // }
+
+
+
+const menuBtn = document.querySelector("#menu-btn")
+const menu = document.querySelector(".menu")
+const body = document.querySelector("body")
+const containerBody = document.querySelector(".container-1-body")
+
+
+menuBtn.addEventListener("click", () =>{
+    menu.classList.toggle("show")
+    body.classList.toggle("hide-scroll")
+    containerBody.classList.toggle("hide-body")
+
+})
+
+menuBtn.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevents the page from jumping when clicking the link
+
+    // Toggle text content
+    if (this.innerText === "menu") {
+        this.innerText = "close";
+    } else {
+        this.innerText = "menu";
+    }
+});
+
+
+
+
+
+
+
+
+
+
+
 
 
 
