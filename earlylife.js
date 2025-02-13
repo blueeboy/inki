4025a102fdc9aee50ce640c24bc18c8278b3a2d3
blueeboy <148228902+blueeboy@users.noreply.github.com>
2025-02-13 19:35:55 +0100

    Add files via upload

--- earlylife.js
+++ earlylife.js
@@ -1,1 +1,23 @@
+const menuBtn = document.querySelector("#menu-btn")
+const menu = document.querySelector(".menu")
+const body = document.querySelector("body")
+const containerBody = document.querySelector(".container-1-body")
 
+
+menuBtn.addEventListener("click", () =>{
+    menu.classList.toggle("show")
+    body.classList.toggle("hide-scroll")
+    containerBody.classList.toggle("hide-body")
+
+})
+
+menuBtn.addEventListener("click", function(event) {
+    event.preventDefault(); // Prevents the page from jumping when clicking the link
+
+    // Toggle text content
+    if (this.innerText === "menu") {
+        this.innerText = "close";
+    } else {
+        this.innerText = "menu";
+    }
+});
